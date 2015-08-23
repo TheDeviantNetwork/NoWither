e570b1800e90a4df4034d124c0b159659cae757e
thomas15v <tvanmellaerts@gmail.com>
2015-08-23 17:17:41 +0200

    Inital commit

---
+++ src/main/java/com/thedeviantnetwork/nowither/NoWitherPlugin.java
@@ -0,0 +1,39 @@
+package com.thedeviantnetwork.nowither;
+
+import com.palmergames.bukkit.towny.Towny;
+import com.palmergames.bukkit.towny.object.Coord;
+import com.palmergames.bukkit.towny.object.TownyWorld;
+import org.bukkit.Location;
+import org.bukkit.entity.EntityType;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.bukkit.event.entity.EntityEvent;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class NoWitherPlugin extends JavaPlugin implements Listener {
+
+    private Towny towny;
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+        getServer().getPluginManager().registerEvents(this,this);
+    }
+
+    @EventHandler
+    public void onCreatureSpawn(EntityEvent event){
+        if(event.getEntityType().equals(EntityType.WITHER) && inprotectedland(event.getEntity().getLocation())){
+            ((CreatureSpawnEvent) event).setCancelled(true);
+        }
+    }
+
+    public boolean inprotectedland(Location location) {
+        try{
+            TownyWorld world = this.towny.getTownyUniverse().getWorldMap().get(location.getWorld().getName());
+            return world.getTownBlock(Coord.parseCoord(location)) != null;
+        }catch (Exception e){}
+        return false;
+    }
+}
